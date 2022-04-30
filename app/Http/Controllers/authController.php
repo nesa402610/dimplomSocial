c05b39717af6d00823726c51034b73e56a7a6d1c
nesa402610 <argyleferris@gmail.com>
2022-04-30 16:27:34 +0300

    создан контроллер для аутентификации и правки для sanctum в Kernel

---
+++ app/Http/Controllers/authController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class authController extends Controller
+{
+    public function createAccount(Request $request) {
+
+        $user = new User;
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->birthday = $request->birthday;
+        $user->phone = $request->phone;
+
+        return response()->json($user);
+    }
+    public function login(Request $request) {
+
+        return response()->json('authed');
+    }
+}
