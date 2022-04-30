34a26aa10e984d2e5c8b58f1f3025d84ad4473eb
nesa402610 <argyleferris@gmail.com>
2022-04-30 13:39:37 +0300

    Шаблон приложения на реакте

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-require('./bootstrap');
+import {BrowserRouter} from "react-router-dom";
+import Index from "./index";
+import * as ReactDOM from 'react-dom/client';
+import './app.css';
+import {Provider} from "react-redux";
+import {store} from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById('paisen'));
+
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Index/>
+        </BrowserRouter>
+    </Provider>
+);
