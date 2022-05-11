38b62dc1e655e5d3c51110ef8328a5803a445c06
nesa402610 <argyleferris@gmail.com>
2022-05-11 17:23:33 +0300

    title и ошибки

--- resources/js/pages/register.jsx
+++ resources/js/pages/register.jsx
@@ -1,36 +1,54 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
+import {Helmet} from "react-helmet";
 
 const Register = () => {
+    const navigate = useNavigate();
     const [regData, setRegData] = useState({
-        firstname: '',
+        name: '',
         lastname: '',
         email: '',
+        phone: '',
         password: '',
         password_confirmation: '',
+    });
+    const [error, setError] = useState({
+        name: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        password: '',
     });
     const createAcc = (e) => {
         e.preventDefault();
         axios.post('creac', regData).then(r => {
-            console.log(r.data, 'resp');
+            navigate('/linkin');
         }).catch(err => {
-            console.log(err.data, 'err');
+            setError(err.response.data.errors);
         });
         console.log(regData, 'log');
     };
     return (
         <div className={'px-4 py-2'}>
+            <Helmet>
+                <title>Создание аккаунта</title>
+            </Helmet>
             <h2 className={'text-slate-100 text-3xl text-center font-bold mb-8'}>Вход в аккаунт</h2>
             <form onSubmit={e => createAcc(e)} className={'flex-col flex gap-3'}>
                 <label>
                     <span className="block text-sm font-medium text-slate-300">Имя </span>
                     <input type="text"
                            required
-                           name="firstname"
-                           onChange={event => setRegData({...regData, firstname: event.target.value})}
+                           name="name"
+                           onChange={event => setRegData({...regData, name: event.target.value})}
                            className="mt-1 px-3 py-2 bg-slate-700 border shadow-sm border-slate-900 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 text-slate-100"
                            placeholder="Изя"/>
+                    {error
+                        ?
+                        <div className="text-red-500 text-sm mt-1">{error.name}</div>
+                        : ''
+                    }
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-300">Фамилия </span>
@@ -40,6 +58,11 @@ const Register = () => {
                            onChange={event => setRegData({...regData, lastname: event.target.value})}
                            className="mt-1 px-3 py-2 bg-slate-700 border shadow-sm border-slate-900 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 text-slate-100"
                            placeholder="Ибрагимов"/>
+                    {error
+                        ?
+                        <div className="text-red-500 text-sm mt-1">{error.lastname}</div>
+                        : ''
+                    }
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-300">Email </span>
@@ -49,6 +72,25 @@ const Register = () => {
                            onChange={event => setRegData({...regData, email: event.target.value})}
                            className="mt-1 px-3 py-2 bg-slate-700 border shadow-sm border-slate-900 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 text-slate-100"
                            placeholder="you@example.com"/>
+                    {error
+                        ?
+                        <div className="text-red-500 text-sm mt-1">{error.email}</div>
+                        : ''
+                    }
+                </label>
+                <label>
+                    <span className="block text-sm font-medium text-slate-300">Номер телефона </span>
+                    <input type="phone"
+                           required
+                           name="phone"
+                           onChange={event => setRegData({...regData, phone: event.target.value})}
+                           className="mt-1 px-3 py-2 bg-slate-700 border shadow-sm border-slate-900 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 text-slate-100"
+                           placeholder="0123456789"/>
+                    {error
+                        ?
+                        <div className="text-red-500 text-sm mt-1">{error.phone}</div>
+                        : ''
+                    }
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-300">Пароль</span>
@@ -58,6 +100,11 @@ const Register = () => {
                            onChange={event => setRegData({...regData, password: event.target.value})}
                            className="mt-1 px-3 py-2 bg-slate-700 border shadow-sm border-slate-900 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 text-slate-100"
                     />
+                    {error
+                        ?
+                        <div className="text-red-500 text-sm mt-1">{error.password}</div>
+                        : ''
+                    }
                 </label>
                 <label>
                     <span className="block text-sm font-medium text-slate-300">Подтверждение пароля</span>
