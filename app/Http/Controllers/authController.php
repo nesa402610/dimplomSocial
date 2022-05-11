488a3a730bfc36a1d8f64be34aa9de47baa6cde2
nesa402610 <argyleferris@gmail.com>
2022-05-11 17:21:14 +0300

    Регистрация и логин

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -4,27 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class authController extends Controller
 {
     public function loginCheck() {
+        return auth()->user();
 
     }
     public function createAccount(Request $request) {
+        $request->validate([
+            'name' => 'required|max:30',
+            'lastname' => 'required|max:30',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|confirmed|min:8|max:40',
+            'phone' => 'required|min:11|max:11',
+        ]);
 
         $user = new User;
-        $user->firstname = $request->firstname;
+
+        $user->name = $request->name;
         $user->lastname = $request->lastname;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
-        $user->birthday = $request->birthday;
         $user->phone = $request->phone;
 
-        return response()->json($user);
-    }
-    public function login(Request $request) {
+        $user->save();
 
-        return response()->json('authed');
+        return $user;
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+//            $request->session()->regenerate();
+            $token = $request->user()->createToken('token-name');
+            return response()->json(['token' => $token->plainTextToken, 'user' => Auth::user()], 200);
+        }
+        throw ValidationException::withMessages([
+            'login' => ['Invalid data']
+        ]);
     }
 }
