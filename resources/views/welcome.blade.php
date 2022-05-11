0d0c7b563a2112ad7709568dd1c9687e90b489f0
nesa402610 <argyleferris@gmail.com>
2022-05-11 17:22:56 +0300

    Изменен title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="js/app.js" defer></script>
-    <title>Title</title>
+    <title>Дипломная соцсеть</title>
 </head>
 <body>
 <div id="paisen"></div>
