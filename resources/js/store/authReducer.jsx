0f4cf5d1539474326589c2a24f2bec032b9040dd
nesa402610 <argyleferris@gmail.com>
2022-05-12 13:45:14 +0300

    authReducer.jsx

--- resources/js/store/authReducer.jsx
+++ resources/js/store/authReducer.jsx
@@ -1,14 +1,18 @@
 const defaultState = {
-    authed: false
+    authed: false,
+    user: {}
 };
 
 const LOG_IN = 'LOG_IN';
 const LOG_OUT = 'LOG_OUT';
+const GET_USER = 'GET_USER'
 
 export const authReducer = (state = defaultState, action) => {
     switch (action.type) {
         case LOG_IN:
             return {...state, authed: action.payload};
+        case GET_USER:
+            return {...state, user: action.payload}
         case LOG_OUT:
             return {...state, authed: action.payload};
         default:
@@ -18,3 +22,4 @@ export const authReducer = (state = defaultState, action) => {
 
 export const loginAction = (payload) => ({type: LOG_IN, payload});
 export const logoutAction = (payload) => ({type: LOG_OUT, payload});
+export const getUserAction = (payload) => ({type: GET_USER, payload});
