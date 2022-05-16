7fe278951b7a74d739b12911b8370bf86bd8a563
nesa402610 <argyleferris@gmail.com>
2022-05-16 14:37:20 +0300

    Просто главная

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Home = () => {
+    return (
+        <h1 className={'text-white text-4xl text-center'}>Это главная страница, но она пуста</h1>
+    )
+};
+
+export default Home;
