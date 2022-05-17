09b04e1a53c1c38fd45fd72474b43c9581c11206
nesa402610 <argyleferris@gmail.com>
2022-05-17 16:08:20 +0300

    Поиск по юзерам

--- resources/js/pages/search.jsx
+++ resources/js/pages/search.jsx
@@ -3,28 +3,42 @@ import axios from "axios";
 import {NavLink} from "react-router-dom";
 
 const Search = () => {
-    const [users, setUsers] = useState('');
+    const [users, setUsers] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
+
+
 
     useEffect(() => {
-        axios.get('/systems/useall').then(r => setUsers(r.data));
+        axios.get('/systems/useall').then(r => {
+            setUsers(r.data);
+        });
     }, []);
+    const filtered = users.filter(user => user.name.toLowerCase().includes(searchValue.toLowerCase()));
     return (
-        <div className={'px-4 text-slate-300 mt-4'}>
+        <div className={'flex flex-col px-4 text-slate-300 mt-4 gap-4'}>
+            <div>
+                <input type="text" value={searchValue}
+                       placeholder={'Кого-то ищешь?'}
+                       onChange={e => setSearchValue(e.target.value)}/>
+            </div>
             <div className={'flex flex-col gap-4'}>
-                {(users)
-                    ?
-                    users.map((user) =>
-                        <NavLink to={'/id' + user.id} className={'flex gap-4 p-2 rounded-md border-2 bg-gray-700 border-slate-800'}>
-                            <div className={'h-20 w-20 rounded-full overflow-hidden'}>
-                                <img src={user.photo} alt=""/>
-                            </div>
-                            <div>
-                                <div>
-                                    {user.name} {user.lastname}
+                {filtered.map((user) => {
+                        return (
+                            <NavLink to={'/id' + user.id}
+                                     key={user.id}
+                                     className={'flex gap-4 p-2 rounded-md border-2 bg-gray-700 border-slate-800'}>
+                                <div className={'h-20 w-20 rounded-full overflow-hidden'}>
+                                    <img src={user.photo} alt=""/>
                                 </div>
-                            </div>
-                        </NavLink>
-                    ) : ''}
+                                <div>
+                                    <div>
+                                        {user.name} {user.lastname}
+                                    </div>
+                                </div>
+                            </NavLink>
+                        );
+                    }
+                )}
             </div>
         </div>
     );
