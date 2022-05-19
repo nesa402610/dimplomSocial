e6810ba1d3a5bdd11c021d67e69d995c4e5fda47
nesa402610 <argyleferris@gmail.com>
2022-05-19 11:37:28 +0300

    Серверное обновление смены данных пользователя

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUndefinedFieldInspection */
 
 namespace App\Http\Controllers;
 
@@ -6,19 +6,18 @@ use App\Models\friend;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
-class UserController extends Controller
-{
-    public function getUser(Request $request)
-    {
+class UserController extends Controller {
+    public function getUser(Request $request) {
         $user = User::find($request->ID);
         $friends = friend::where('user_id', $request->ID)->get();
         $authUser = Auth::user();
         return response(['user' => $user, 'friends' => $friends, 'me' => $authUser]);
 //            return [$request->ID, Auth::user()->id];
     }
-    public function getUsers()
-    {
+
+    public function getUsers() {
         $user = User::all();
 //        $friends = friend::where('user_id', $request->ID)->get();
 //        $authUser = Auth::user();
@@ -26,15 +25,13 @@ class UserController extends Controller
 //            return [$request->ID, Auth::user()->id];
     }
 
-    public function updateStatus(Request $request)
-    {
+    public function updateStatus(Request $request) {
         $User = Auth::user();
         $User->status = $request->status;
         $User->save();
     }
 
-    public function updatePhoto(Request $request)
-    {
+    public function updatePhoto(Request $request) {
         $User = Auth::user();
         $ext = $request->file('photo')->extension();
         $rand = rand(0, 10);
@@ -44,8 +41,7 @@ class UserController extends Controller
         return response()->json(['photo' => $User->photo]);
     }
 
-    public function addToFriends(Request $request)
-    {
+    public function addToFriends(Request $request) {
 
         if (Auth::user()->id == $request->friend_id) {
             return response('Нельзя дружить с сами собой!');
@@ -56,5 +52,29 @@ class UserController extends Controller
             $friend->save();
             return response('success', 200);
         }
+    }
+
+    public function basicInfo(Request $request) {
+        $user = Auth::user();
+        if ($request->name) {
+            $user->name = $request->name;
+            $user->lastname = $request->lastname;
+        } else if ($request->birthday) {
+            $user->birthday = $request->birthday;
+        } else if ($request->phone) {
+            $user->phone = $request->phone;
+        } else if ($request->gender) {
+            $user->gender = $request->gender;
+        } else if ($request->email) {
+            $user->email = $request->email;
+        } else if ($request->password) {
+            $request->validate([
+                'password' => 'required|confirmed|min:8|max:40',
+            ]);
+            $user->password = Hash::make($request->password);
+        }
+        $user->save();
+
+        return response(["ok"]);
     }
 }
